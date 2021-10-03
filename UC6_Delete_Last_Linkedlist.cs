c801485c826b878af8c853709f445cd533edd96b
Zeehan0133 <zyne0133@gmail.com>
2021-10-04 00:25:11 +0530

    Create a  linked list to delete last element

---
+++ UC6_Delete_Last_Linkedlist.cs
@@ -0,0 +1,72 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day14_Stack_QueueAssignment
+{
+    class UC6_Delete_Last_Linkedlist
+    {
+        internal Node head;
+
+        internal void Add(int data)
+        {
+            Node node = new Node(data);
+            if (this.head == null)
+            {
+                this.head = node;
+            }
+            else
+            {
+                Node temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+            }
+            Console.WriteLine("{0} inserted into LinkedList", node.data);
+        }
+
+        internal void Display()
+        {
+            Node temp = this.head;
+            if (temp == null)
+            {
+                Console.WriteLine("LinkedList is empty");
+                return;
+            }
+            while (temp != null)
+            {
+                Console.WriteLine(temp.data + " ");
+                temp = temp.next;
+            }
+
+        }
+
+        internal Node PopLast()
+        {
+            if (head == null)
+                return null;
+            if (head.next == null)
+                return null;
+            Node newNode = head;
+            while (newNode.next.next != null)
+            {
+                newNode = newNode.next;
+            }
+            newNode.next = null;
+            return head;
+        }
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
