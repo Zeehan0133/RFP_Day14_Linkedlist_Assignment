27c5ccf1038c9a1128210a1116beb994fa58387f
Zeehan0133 <zyne0133@gmail.com>
2021-10-04 00:28:49 +0530

    Create a  linked list to insert 40 after 30

---
+++ UC8_Insert_Linkedlist.cs
@@ -0,0 +1,70 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day14_Stack_QueueAssignment
+{
+    class UC8_Insert_Linkedlist
+    {
+        internal Node head;
+        internal void Add(int data)
+        {
+            Node node = new Node(data);
+            if (this.head == null)
+                this.head = node;
+            else
+            {
+                Node temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+            }
+            Console.WriteLine("\n{0} inserted into the Linked List", node.data);
+        }
+
+        internal bool Search(Node head, int x)
+        {
+            Node current = head; // Initialize current
+            while (current != null)
+            {
+                if (current.data == x)
+                    return true; // data found
+                current = current.next;
+            }
+            return false; // data not found
+        }
+
+        internal void InsertAfter(Node prev_node, int new_data)
+        {
+            //Check if the given Node is null
+            if (prev_node == null)
+            {
+                Console.WriteLine("The given previous node cannot be null");
+
+                return;
+            }
+            Node new_node = new Node(new_data);
+            new_node.next = prev_node.next;
+            prev_node.next = new_node;
+        }
+
+        internal void Display()
+        {
+            Node temp = this.head;
+            if (temp == null)
+            {
+                Console.WriteLine("Linked list is empty");
+                return;
+            }
+            while (temp != null)
+            {
+                Console.Write(temp.data + " ");
+                temp = temp.next; //temp=null
+            }
+        }
+    }
+
+
+}
