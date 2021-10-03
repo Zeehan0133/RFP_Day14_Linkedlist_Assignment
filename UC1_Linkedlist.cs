54276ccf1a5c7ae82006eb648ec02f7fd5a5801c
Zeehan0133 <zyne0133@gmail.com>
2021-10-04 00:10:20 +0530

    Create a simple linked list

---
+++ UC1_Linkedlist.cs
@@ -0,0 +1,54 @@
+using System;
+
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day14_Stack_QueueAssignment
+{
+    class UC1_LinkedList
+    {
+       
+            internal Node head;
+
+            internal void Add(int data)
+            {
+                Node node = new Node(data);
+                if (this.head == null)
+                {
+                    this.head = node;
+                }
+                else
+                {
+                    Node temp = head;
+                    while (temp.next != null)
+                    {
+                        temp = temp.next;
+                    }
+                    temp.next = node;
+                }
+                Console.WriteLine("{0} inserted into LinkedList", node.data);
+            }
+
+            internal void Display()
+            {
+                Node temp = this.head;
+                if (temp == null)
+                {
+                    Console.WriteLine("LinkedList is empty");
+                    return;
+                }
+                while (temp != null)
+                {
+                    Console.WriteLine(temp.data + " ");
+                    temp = temp.next;
+                }
+            }
+
+
+
+
+
+        
+
+    }
+}   
