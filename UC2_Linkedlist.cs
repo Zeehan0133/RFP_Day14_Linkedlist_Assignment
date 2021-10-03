dfdd8c941f98bf3dc78289a8031de77b7d6952ad
Zeehan0133 <zyne0133@gmail.com>
2021-10-04 00:16:36 +0530

    Create a  linked list of 30 56 70

---
+++ UC2_Linkedlist.cs
@@ -0,0 +1,48 @@
+using Day14_Stack_QueueAssignment;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day14_Stack_QueueAssignment
+{
+    class UC2_LinkedList
+    {
+        internal Node head; //new
+        internal void Add(int data)
+        {
+            Node node = new Node(data);
+            if (this.head == null)
+                this.head = node;
+            else
+            {
+                Node temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+            }
+            Console.WriteLine("{0} inserted into the linked list", node.data);
+        }
+
+
+
+        internal void Display()
+        {
+            Node temp = this.head;
+            if (temp == null)
+            {
+                Console.WriteLine("Linked list is empty");
+                return;
+            }
+            while (temp != null)
+            {
+                Console.Write(temp.data + " ");
+                temp = temp.next; //temp=null
+            }
+        }
+        
+
+
+    }
+}
