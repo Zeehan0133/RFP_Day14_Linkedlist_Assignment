e020732d060814af0787c7966df29b76ad44de84
Zeehan0133 <zyne0133@gmail.com>
2021-10-04 14:25:39 +0530

    Stack and Queue Program.cs

---
+++ stackProgram.cs
@@ -0,0 +1,40 @@
+using System;
+using Data_Structures_Demo.linked_list1;
+
+using Data_Structures_Demo.Stack_Using_linklist;
+
+namespace Data_Structures_Demo
+{
+    class Program
+    {
+       public static void Main(string[] args)
+        {
+            
+
+
+            #endregion
+
+            //#region QueueUsinglinkedList
+            //LinkedListQueue linkedListQueue = new LinkedListQueue();
+            //linkedListQueue.Enqueue(56);
+            //linkedListQueue.Enqueue(30);
+            //linkedListQueue.Enqueue(70);
+            //linkedListQueue.Display();// this.top=this.top.next
+            //#endregion
+
+            //#region StackUsingLinkedList
+            //LinkedListStack linkedListStack = new LinkedListStack();
+            //linkedListStack.Push(70);
+            //linkedListStack.Push(30);
+            //linkedListStack.Push(56);
+            //linkedListStack.Display();
+            //linkedListStack.Peek(); //top.data
+            //linkedListStack.Pop();
+            //linkedListStack.IsEmpty();
+            //linkedListStack.Display();
+            //#endregion
+
+            Console.ReadKey();
+        }
+    }
+}
